18f6976cbb93ec0a2a85b1a99aafbc3bdc0dfb4e
ginesh29 <ginesh29@gmail.com>
2020-01-22 00:25:32 +0530

    fullcalender

--- src/components/appointment-calendar.js
+++ src/components/appointment-calendar.js
@@ -7,10 +7,8 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import '@fullcalendar/core/main.css';
 import interactionPlugin from "@fullcalendar/interaction";
 
-export default class AppointmentCalendar extends Component
-{
-    constructor(props)
-    {
+export default class AppointmentCalendar extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             appointments: [],
@@ -24,14 +22,11 @@ export default class AppointmentCalendar extends Component
         this.repository = new repository();
         this.helper = new helper();
     }
-    getAppointments = () =>
-    {
+    getAppointments = () => {
         const { filterString, sortString, includeProperties, controller } = this.state;
-        return this.repository.get(controller, `filter=${ filterString }&sort=${ sortString }&includeProperties=${ includeProperties }`, this.messages)
-            .then(res =>
-            {
-                res && res.data.map(item =>
-                {
+        return this.repository.get(controller, `filter=${filterString}&sort=${sortString}&includeProperties=${includeProperties}`, this.messages)
+            .then(res => {
+                res && res.data.map(item => {
                     item.title = item.patient.firstname;
                     item.description = item.patient.fullname;
                     item.start = this.helper.formatFullcalendarDate(item.date);
@@ -44,48 +39,76 @@ export default class AppointmentCalendar extends Component
                 });
             })
     }
-    componentDidMount()
-    {
-        const month = this.helper.getMonthFromDate();
-        const year = this.helper.getYearFromDate();
-        const filter = `Date.Month-eq-{${ month }} and Date.Year-eq-{${ year }}`;
-        this.setState({ filterString: filter }, () =>
-        {
-            this.getAppointments();
-        });
+    // componentDidMount() {
+    //     const month = this.helper.getMonthFromDate();
+    //     const year = this.helper.getYearFromDate();
+    //     const filter = `Date.Month-eq-{${month}} and Date.Year-eq-{${year}}`;
+    //     this.setState({ filterString: filter }, () => {
+    //         this.getAppointments();
+    //     });
+    // }
+
+    getAppointments = (fetchInfo, successCallback) => {
+        const { filterString, sortString, includeProperties, controller } = this.state;
+        this.repository.get(controller, `filter=${filterString}&sort=${sortString}&includeProperties=${includeProperties}`, this.messages)
+            .then(res => {
+                let appointments = res && res.data.map(function (item) {
+                    return {
+                        title: item.patient.firstname,
+                        description: item.patient.fullname,
+                        start: "2019-01-01",// this.helper.formatFullcalendarDate(item.date),
+                        color: appointmentTypeEnum[item.appointmentType.toUpperCase()].color
+                    };
+                });
+                successCallback(appointments)
+            });
     }
-    render()
-    {
-        const { appointments } = this.state;
+    getCalendarData(fetchInfo, successCallback) {
+        const { filterString, sortString, includeProperties, controller } = this.state;
+        try {
+
+            let year = new Date().getFullYear();
+            let month = new Date().getMonth() + 1;
+
+            if (fetchInfo) {
+                year = new Date(fetchInfo.start).getFullYear();
+                month = new Date(fetchInfo.start).getMonth() + 1;
+            }
+
+            this.repository.get(controller, `filter=${filterString}&sort=${sortString}&includeProperties=${includeProperties}`, this.messages)
+                .then(res => {
+                    let appointments = res && res.data.map(function (item) {
+                        return {
+                            title: item.patient.firstname,
+                            description: item.patient.fullname,
+                            start: "2019-01-01",// this.helper.formatFullcalendarDate(item.date),
+                            color: appointmentTypeEnum[item.appointmentType.toUpperCase()].color
+                        };
+                    });
+                    successCallback(appointments)
+                });
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    render() {
         const options = {
+            hiddenDays: [0],
             height: 750,
             plugins: [dayGridPlugin, interactionPlugin],
             defaultDate: '2020-01-01',//this.helper.formatFullcalendarDate(),//new Date(),
             editable: true,
-            dateClick: function ()
-            {
-                alert()
+            dateClick: function () {
+                //alert()
             }
-            // eventRender: function (event, element)
-            // {
-            //     // element.popover({
-            //     //     animation: true,
-            //     //     delay: 300,
-            //     //     content: '<b>Inicio</b>:' + event.start + "<b>Fin</b>:" + event.end,
-            //     //     trigger: 'hover'
-            //     // });
-            //     // var tooltip = new Tooltip(info.el, {
-            //     //     title: info.event.extendedProps.description,
-            //     //     placement: 'top',
-            //     //     trigger: 'hover',
-            //     //     container: 'body'
-            //     // });
-            // }
         }
         return (
-            <div className="row">
+            <div className="row" >
                 <div className="col-md-12">
-                    <FullCalendar options={options} events={appointments} />
+                    <FullCalendar options={options} events={
+                        (fetchInfo, successCallback, failureCallback) => this.getCalendarData(fetchInfo, successCallback, failureCallback)
+                    } />
                 </div>
             </div>
         );
