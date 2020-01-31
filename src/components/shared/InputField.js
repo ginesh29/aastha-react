dbbdf05b58e38022d384b7a0ce35688c4625f811
Ginesh Tandel <ginesh29@gmail.com>
2020-01-31 18:53:00 +0530

    input

--- src/components/shared/InputField.js
+++ src/components/shared/InputField.js
@@ -15,85 +15,53 @@ export default class InputField extends Component
     let errorClass = validationErrors[name] ? "error" : "";
     let propClassName = className ? className : "";
     let finalClassName = `${ propClassName } ${ errorClass }`;
-    if (controlType === "input-group-addon") {
-      return (
-        <div className="form-group">
-          <label>{title}</label>
+    return (
+      <div className="form-group">
+        <label>{title}</label>
+        {
+          controlType === "input-group-addon" &&
           <div className="p-inputgroup">
             <InputText name={name} value={value} className={finalClassName} placeholder={"Enter " + title} onChange={onChange} readOnly={readOnly} keyfilter={keyfilter} maxLength={maxLength} ref={ref} />
             <span className="p-inputgroup-addon">
               <i className={"fa " + groupIcon}></i>
             </span>
           </div>
-          <span className="error">{validationErrors[name]}</span>
-        </div>
-      );
-    }
-    else if (controlType === "dropdown") {
-      return (
-        <div className="form-group">
-          <label>{title}</label>
+        }
+        {
+          controlType === "dropdown" &&
           <Dropdown name={name} value={value} options={options} className={finalClassName} placeholder={"Select " + title} optionLabel={optionLabel} onChange={onChange} filter={filter} filterPlaceholder={"Please enter 1 or more charactor to Search " + title} filterBy={filterBy} showClear={true} ref={ref} />
-          <span className="error">{validationErrors[name]}</span>
-        </div>
-      );
-    }
-    else if (controlType === "select2") {
-      return (
-        <div className="form-group">
-          <label>{title}</label>
+        }
+        {
+          controlType === "select2" &&
           <AsyncCreatableSelect defaultOptions name={name} value={value} onCreateOption={onCreateOption} createOptionPosition="first" loadOptions={loadOptions} className={finalClassName} isClearable={true} isLoading={true} onChange={onChange} onInputChange={onInputChange} />
-          <span className="error">{validationErrors[name]}</span>
-        </div>
-      );
-    }
-    else if (controlType === "datepicker") {
-      return (
-        <div className="form-group">
-          <label>{title}</label>
+        }
+        {
+          controlType === "datepicker" &&
           <div className="p-inputgroup">
             <Calendar name={name} value={value} showIcon={true} className={propClassName} inputClassName={errorClass} placeholder={"Enter " + title} onChange={onChange} dateFormat="dd/mm/yy" readOnlyInput={true} timeOnly={timeOnly} hourFormat={hourFormat} icon={icon} dataKey={dataKey} minDate={minDate} ref={ref} disabled={disabled} readOnly={readOnly} />
             <span className="p-inputgroup-addon">
               <i className={"fa " + groupIcon}></i>
             </span>
           </div>
-          <span className="error">{validationErrors[name]}</span>
-        </div>
-      );
-    }
-    else if (controlType === "autocomplete") {
-      return (
-        <div className="form-group">
-          <label>{title}</label>
+        }
+        {
+          controlType === "autocomplete" &&
           <AutoComplete name={name} value={value} className={finalClassName} inputClassName={errorClass} placeholder={"Enter " + title} onChange={onChange} suggestions={suggestions} completeMethod={completeMethod} onFocus={onFocus} minLength={minLength} ref={ref} />
-          <span className="error">{validationErrors[name]}</span>
-        </div>
-      );
-    }
-    else if (controlType === "multiselect") {
-      return (
-        <div className="form-group">
-          <label>{title}</label>
+        }
+        {
+          controlType === "multiselect" &&
           <MultiSelect name={name} value={value} className={finalClassName} options={options} onChange={onChange} filter={filter} placeholder={"Select " + title} />
-          <span className="error">{validationErrors[name]}</span>
-        </div>
-      );
-    }
-    else if (controlType === "textarea") {
-      return (
-        <div className="form-group">
-          <label>{title}</label>
+        }
+        {
+          controlType === "textarea" &&
           <InputTextarea autoResize={true} name={name} value={value} className={finalClassName} placeholder={"Enter " + title} onChange={onChange} onInput={onInput} disabled={disabled} keyfilter={keyfilter} maxLength={maxLength} onFocus={this.onFocus} autoFocus={this.autoFocus} ref={ref} />
-          <span className="error">{validationErrors[name]}</span>
-        </div>
-      );
-    }
-    return (
-      <div className="form-group">
-        <label>{title}</label>
-        <InputText name={name} value={value} className={finalClassName} placeholder={"Enter " + title} onChange={onChange} onInput={onInput} disabled={disabled} keyfilter={keyfilter} maxLength={maxLength} onFocus={this.onFocus} autoFocus={this.autoFocus} ref={ref} />
+        }
+        {
+          !controlType &&
+          < InputText name={name} value={value} className={finalClassName} placeholder={"Enter " + title} onChange={onChange} onInput={onInput} disabled={disabled} keyfilter={keyfilter} maxLength={maxLength} onFocus={this.onFocus} autoFocus={this.autoFocus} ref={ref} />
+        }
         <span className="error">{validationErrors[name]}</span>
       </div>
-    );
+    )
   }
 }
